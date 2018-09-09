6f98d9ab4012c8e9ff973eb7a71911237c710b75
Luke Meyrick <luke@meyrick.eu>
2018-09-09 15:54:53 +0100

    Move DNS to own env

---
+++ environments/route53/data.tf
@@ -0,0 +1,3 @@
+data "aws_ssm_parameter" "route53-delegation-set" {
+  name = "route53-delegation-set"
+}
---
+++ environments/route53/variables.tf
@@ -0,0 +1,8 @@
+variable "domain" {
+  default = "meyrick"
+}
+
+variable "delegation_set" {
+  default = "N3SLZFK3V59WGF"
+}
+
