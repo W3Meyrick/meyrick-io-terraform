74620197685eacfb218ba73d70cfc0f7696d1343
Luke Meyrick <luke@meyrick.eu>
2018-10-05 21:38:31 +0100

    Module path

--- environments/route53/main.tf
+++ environments/route53/main.tf
@@ -63,7 +63,7 @@ resource "aws_route53_zone" "secondary" {
 }
 
 module "route53_o365" {
-  source = "github.com/w3meyrick/meyrick-io-tf-modules/tree/master/modules/route53-o365"
+  source = "git@github.com/w3meyrick/meyrick-io-tf-modules/modules/route53-o365.git"
 
   domain           = "meyrick.io"
   zone_id          = "${aws_route53_zone.secondary.zone_id}"
